f94a89da173ab1a5f57a55e5afcdb3adf4ab49ea
Thanh Long Tran <thanh-long.tran@etu.unice.fr>
2017-10-03 23:19:05 +0200

    Initial Commit

---
+++ TP3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TP3
+//
+//  Created by Thanh Long Tran on 03/10/2017.
+//  Copyright © 2017 Thanh Long Tran. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
